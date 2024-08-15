b4961b7a30845ad67b81bd2a96130cc15617fb51
LXiangEthan <166964157+LXiangEthan@users.noreply.github.com>
2024-08-15 14:40:27 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -8,17 +8,23 @@ const https = require("https")
 let server = http.createServer()
 
 server.on("request",(req,res)=>{
-    let myUrl = new URL(req.url,"http://localhost:3000")
+    try {
+    let myUrl = new URL(req.url,"http://localhost:9000")
     res.writeHead(200, {
         "Content-Type": "image/webp"
-    })
+        })
 
     switch(myUrl.pathname){
         case '/anime':
+            console.log("来自"+req.socket.remoteAddress+"的使用者:")
             httpGet(res)
             break
         default:
             res.end("404")
+        }
+    }
+    catch{
+        response.end("error")
     }
 })
 
@@ -27,23 +33,30 @@ server.listen(9000,()=>{
 })
 
 function httpGet(response){
+
     let data = ""
     let page = Math.ceil(Math.random() * 190)
-    try {
-        https.get(`https://api.codelife.cc/wallpaper/wallhaven?lang=cn&page=${page}&size=20&q=id:5`, (res) => {
+        let address = `https://api.codelife.cc/wallpaper/wallhaven?lang=cn&page=${page}&size=24&q=id:5`
+        https.get(address, (res) => {
             res.on("data", (chunk) => {
                 data += chunk
             })
             res.on("end", () => {
-                let id = Math.floor(Math.random() * ((JSON.parse(data)).data.length))
-                let randomId = (JSON.parse(data)).data[id].raw
+                if((JSON.parse(data))?.data?.length === 0){
+                    let defaultUrl = "https://files.codelife.cc/wallhaven/full/9m/wallhaven-9mqqq8.png?x-oss-process=image/resize,limit_0,m_fill,w_2560,h_1440/quality,Q_92/format,webp"
+                    https.get(defaultUrl, (ress) => {
+                        console.log("默认请求地址-->"+defaultUrl)
+                        ress.pipe(response)
+                    })
+                }
+                else{
+                let id = Math.floor(Math.random() * ((JSON.parse(data))?.data?.length))
+                let randomId = (JSON.parse(data))?.data[id]?.raw
                 https.get(randomId, (ress) => {
+                    console.log("请求地址-->"+randomId)
                     ress.pipe(response)
                 })
+            }
             })
         })
-    }
-    catch{
-        response.end("error")
-    }
 }
